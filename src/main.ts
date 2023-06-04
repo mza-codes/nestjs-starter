61f4aef16f61b45f38d4e34ef34a84ce1f9db3d9
mza-codes <106687236+mza-codes@users.noreply.github.com>
2023-06-04 17:43:56 +0530

    add dtos, whitelist, error route

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 (async function () {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.listen(3000);
 })();
+
+/**
+ * @param { whitelist: boolean } params mentioned in the dtos will only be available!
+ */
